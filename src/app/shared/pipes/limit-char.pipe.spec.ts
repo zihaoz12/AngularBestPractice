092a571e455d65ffe919cec567680dbe8e9971c1
patrick.lin@antra.com <patrick.lin@antra.com>
2019-11-11 17:42:04 -0500

    update lint

--- src/app/shared/pipes/limit-char.pipe.spec.ts
+++ src/app/shared/pipes/limit-char.pipe.spec.ts
@@ -1,24 +1,23 @@
-import { defaultValue, LimitCharPipe } from "./limit-char.pipe";
+import { defaultValue, LimitCharPipe } from './limit-char.pipe';
 
-describe("LimitCharPipe", () => {
+describe('LimitCharPipe', () => {
   beforeEach(() => {});
 
   const limitCharPipe = new LimitCharPipe();
 
-  it("should create", () => {
+  it('should create', () => {
     expect(limitCharPipe).toBeTruthy();
   });
 
   // tslint:disable-next-line: max-line-length
   it(`should return the limit ${defaultValue.limitNum} Char string by default if the input has more than ${defaultValue.limitNum} char string`, () => {
-    const input =
-      "Hello Patrick12321321312312321321312312321312312312313213123123123123";
-    const expectOutput = input.slice(0, defaultValue.limitNum) + "...";
+    const input = 'Hello Patrick123213213123123213213123123213123';
+    const expectOutput = input.slice(0, defaultValue.limitNum) + '...';
     expect(limitCharPipe.transform(input)).toBe(expectOutput);
   });
 
   it(`should return itself by default if the input has no more than ${defaultValue.limitNum} char string`, () => {
-    const input = "Hello Patrick";
+    const input = 'Hello Patrick';
     const expectOutput = input;
     expect(limitCharPipe.transform(input)).toBe(expectOutput);
   });
