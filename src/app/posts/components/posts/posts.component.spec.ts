fb33f099d478565b2f3ed59938c3e37dbe34b996
patrick.lin@antra.com <patrick.lin@antra.com>
2019-11-12 11:45:56 -0500

    update test cases

--- src/app/posts/components/posts/posts.component.spec.ts
+++ src/app/posts/components/posts/posts.component.spec.ts
@@ -31,6 +31,29 @@ import { ModalService } from '../../../shared/services/modal.service';
 //   });
 // });
 
+// describe('PostsComponent', () => {
+//   let component: PostsComponent;
+//   let fixture: ComponentFixture<PostsComponent>;
+
+//   beforeEach(async () => {
+//     TestBed.configureTestingModule({
+//       declarations: [PostsComponent],
+//       providers: [
+//         { provide: PostsService, useValue: PostsServiceStub },
+//         { provide: CommentsService, useValue: CommentsServiceStub },
+//         { provide: ModalService, useValue: ModalServiceStub }
+//       ],
+//       schemas: [CUSTOM_ELEMENTS_SCHEMA]
+//     }).compileComponents();
+//     fixture = TestBed.createComponent(PostsComponent);
+//     component = fixture.debugElement.componentInstance;
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
 const PostsServiceStub = {};
 const CommentsServiceStub = {};
 const ModalServiceStub = {};
@@ -38,22 +61,37 @@ const ModalServiceStub = {};
 describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
-
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [PostsComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         { provide: PostsService, useValue: PostsServiceStub },
-        { provide: CommentsService, useValue: CommentsServiceStub },
-        { provide: ModalService, useValue: ModalServiceStub }
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        { provide: ModalService, useValue: ModalServiceStub },
+        { provide: CommentsService, useValue: CommentsServiceStub }
+      ]
     }).compileComponents();
-    fixture = TestBed.createComponent(PostsComponent);
-    component = fixture.debugElement.componentInstance;
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(PostsComponent);
+    component = fixture.debugElement.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('should have getPosts and clearPosts button', () => {
+    fixture = TestBed.createComponent(PostsComponent);
+    component = fixture.componentInstance;
+    const getPostsBtnElement: HTMLElement = fixture.nativeElement.querySelector(
+      '.btnGetPosts'
+    );
+    expect(getPostsBtnElement).toBeTruthy();
+
+    const clearPostsBtnElement: HTMLElement = fixture.nativeElement.querySelector(
+      '.btnClearPosts'
+    );
+    expect(clearPostsBtnElement).toBeTruthy();
+  });
+
+  it('should display posts list if you click getPosts button', () => {});
 });
--- src/app/posts/services/posts.service.spec.ts
+++ src/app/posts/services/posts.service.spec.ts
@@ -1,46 +1,86 @@
+import { PostsService } from './posts.service';
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController
 } from '@angular/common/http/testing';
-import { PostsService } from './posts.service';
 import { Post } from '../interface/postsAPI.interface';
 
+// import { TestBed } from '@angular/core/testing';
+// import {
+//   HttpClientTestingModule,
+//   HttpTestingController
+// } from '@angular/common/http/testing';
+// import { PostsService } from './posts.service';
+// import { Post } from '../interface/postsAPI.interface';
+
+// describe('PostsService', () => {
+//   let postsService: PostsService;
+//   let httpTestingController: HttpTestingController;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [PostsService],
+//       imports: [HttpClientTestingModule]
+//     });
+
+//     postsService = TestBed.get(PostsService);
+//     httpTestingController = TestBed.get(HttpTestingController);
+//   });
+
+//   it('should be created', () => {
+//     expect(postsService).toBeTruthy();
+//   });
+
+//   it(`should get the Posts data `, () => {
+//     const mockData: Post[] = [
+//       { id: 1, title: '1', body: '1' },
+//       { id: 2, title: '2', body: '2' }
+//     ];
+//     postsService.getPosts().subscribe(data => {
+//       expect(data).toEqual(mockData);
+//     });
+
+//     const req = httpTestingController.expectOne(
+//       postsService.baseUrl + '/posts'
+//     );
+//     expect(req.request.method).toBe('GET');
+//     req.flush(mockData);
+//   });
+
+//   afterEach(() => {
+//     // httpTestingController.verify();
+//   });
+// });
+
 describe('PostsService', () => {
-  let postsService: PostsService;
+  let service: PostsService;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PostsService],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [PostsService]
     });
-
-    postsService = TestBed.get(PostsService);
+    service = TestBed.get(PostsService);
     httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(postsService).toBeTruthy();
+  it('should create', () => {
+    expect(service).toBeTruthy();
   });
 
-  it(`should get the Posts data `, () => {
+  it('should call Posts API() to get Posts Data', () => {
     const mockData: Post[] = [
       { id: 1, title: '1', body: '1' },
       { id: 2, title: '2', body: '2' }
     ];
-    postsService.getPosts().subscribe(data => {
+    service.getPosts().subscribe(data => {
       expect(data).toEqual(mockData);
     });
-
-    const req = httpTestingController.expectOne(
-      postsService.baseUrl + '/posts'
-    );
+    const req = httpTestingController.expectOne(service.baseUrl + '/posts');
     expect(req.request.method).toBe('GET');
     req.flush(mockData);
-  });
-
-  afterEach(() => {
-    // httpTestingController.verify();
   });
 });
